9275390675abd27f6f7d40c580c102902be4f3ff
rialg <me@gastonrial.xyz>
2022-10-16 17:01:55 +0000

    Add temporary persistent storage

--- install.bash
+++ install.bash
@@ -3,9 +3,10 @@
 # Build docker image
 docker image build . --tag quick_link
 # Run the quicklink proxy
-docker run -d --rm --name "quick_link" quick_link:latest
+docker run --mount type=bind,source=/usr/local/redirects.json,target=$(pwd)/static/redirects.json \
+           -d --rm --name "quick_link" quick_link:latest
 # Get the container IP addr
 IP_ADDR=$(docker container inspect quick_link -f '{{ .NetworkSettings.IPAddress }}')
 
 # Add the quick link hostname
-echo "${IP_ADDR}	boozt" >> /etc/hosts
+echo "${IP_ADDR}	<QUICKLINK_ROOT>" >> /etc/hosts
