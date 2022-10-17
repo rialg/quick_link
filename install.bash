ada34d0c248432f30f37fa1dd9e207c5d35df6ae
rialg <me@gastonrial.xyz>
2022-10-17 15:58:40 +0000

    Make container persitent after rebooting the host

--- install.bash
+++ install.bash
@@ -4,7 +4,7 @@
 docker image build . --tag quick_link
 # Run the quicklink proxy
 docker run --mount type=bind,source=/usr/local/redirects.json,target=$(pwd)/static/redirects.json \
-           -d --rm --name "quick_link" quick_link:latest
+           -d --name "quick_link" quick_link:latest
 # Get the container IP addr
 IP_ADDR=$(docker container inspect quick_link -f '{{ .NetworkSettings.IPAddress }}')
 
